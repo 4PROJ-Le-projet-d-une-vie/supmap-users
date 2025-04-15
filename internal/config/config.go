81392bddaf3368b15bd00cb4a03512db194b8fb2
Ewennn <ewen.bosquet@gmail.com>
2025-04-15 23:44:25 +0200

    feat/ Prepare environment variable parsing

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,19 @@
 package config
 
+import (
+	"fmt"
+	"github.com/caarlos0/env/v11"
+)
+
 type Config struct {
-	DB_URL string `env:DB_URL`
-	PORT   string `env:PORT`
+	DbUrl string `env:"DB_URL"`
+	PORT  string `env:"PORT"`
 }
 
 func New() (*Config, error) {
-	return &Config{}, nil
+	cfg, err := env.ParseAs[Config]()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load config: %w", err)
+	}
+	return &cfg, nil
 }
