e9d7238eb200ffe8118e8d85890b494f07bc9333
Ewennn <ewen.bosquet@gmail.com>
2025-04-24 00:35:23 +0200

    feat/ Implement decode JWT function and correctly reimplements Auth middleware

--- internal/api/middlewares.go
+++ internal/api/middlewares.go
@@ -10,6 +10,13 @@ import (
 	"supmap-users/internal/models"
 )
 
+type AuthError struct {
+	Error string `json:"error"`
+}
+
+var invalidToken = &AuthError{Error: "invalid token"}
+var invalidUser = &AuthError{Error: "invalid user"}
+
 func (s *Server) AuthMiddleware() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -20,19 +27,35 @@ func (s *Server) AuthMiddleware() func(http.Handler) http.Handler {
 			}
 
 			token := strings.TrimPrefix(authHeader, "Bearer ")
-			userID, err := decodeJWT(token)
+			userID, err := decodeJWT(token, s.Config.JwtSecret)
 			if err != nil {
-				http.Error(w, "Invalid token", http.StatusUnauthorized)
+				fmt.Println(err)
+				if err := handler.Encode(invalidToken, http.StatusUnauthorized, w); err != nil {
+					s.log.Error("Error encoding response", err)
+					w.WriteHeader(http.StatusInternalServerError)
+				}
 				return
 			}
 
-			user, err := s.users.FindByID(r.Context(), userID)
+			user, err := s.service.GetUserByID(r.Context(), *userID)
 			if err != nil {
-				http.Error(w, "Invalid user", http.StatusUnauthorized)
+				if err := handler.Encode(invalidUser, http.StatusUnauthorized, w); err != nil {
+					s.log.Error("Error while try to retrieve user", err)
+					w.WriteHeader(http.StatusInternalServerError)
+				}
 				return
 			}
 
-			s.log.Debug("user: %+v\n", user) // TODO remove debug
+			if user == nil {
+				if err := handler.Encode(invalidToken, http.StatusUnauthorized, w); err != nil {
+					s.log.Error("User not found", err)
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+				return
+			}
+
+			s.log.Info("User authenticated", "userID", user.ID)
+
 			ctx := context.WithValue(r.Context(), "user", user)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
@@ -60,8 +83,25 @@ func (s *Server) AdminMiddleware() func(http.Handler) http.Handler {
 	}
 }
 
-func decodeJWT(token string) (int64, error) {
-	// TODO decode token here
-	fmt.Println(token)
-	return 1, nil
+func decodeJWT(tokenStr string, secret string) (*int64, error) {
+	claims := jwt.MapClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, fmt.Errorf("error parsing token: %v", err)
+	}
+
+	userId, ok := claims["userId"].(float64)
+	if !ok {
+		return nil, fmt.Errorf("userId is missing or of wrong type")
+	}
+
+	convertedId := int64(userId)
+	return &convertedId, nil
 }
