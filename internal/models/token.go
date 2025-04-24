c3119fb3c98fbd721e8e0c8ffe7209310010249e
Ewennn <ewen.bosquet@gmail.com>
2025-04-25 00:21:35 +0200

    feat/ Create Bun refresh_token structure

---
+++ internal/models/token.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"github.com/uptrace/bun"
+	"time"
+)
+
+type Token struct {
+	bun.BaseModel `bun:"table:refresh_tokens"`
+
+	UserID    int64     `bun:"user_id,pk"`
+	User      *User     `bun:"rel:belongs-to,join:user_id=id"`
+	IP        string    `bun:"ip,notnull"`
+	Token     string    `bun:"token,notnull"`
+	CreatedAt time.Time `bun:"created_at,notnull"`
+	ExpiresAt time.Time `bun:"expires_at,notnull"`
+}
